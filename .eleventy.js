f58cf7ce71f7c9ca362a711af8a5411740abb601
David Bryant <david@disquisitioner.com>
2019-12-30 09:54:32 -0800

    Custom collection to sort apps

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,17 @@ module.exports = function(eleventyConfig) {
   // Add some utility filters
   eleventyConfig.addFilter("dateDisplay", require("./utils/date.js") );
 
+  // custom collection that sorts Applications alphabetically by title
+  // Use: {%- for item in collections.appsAscending -%}
+  // Assumes individual apps will be listed as *.html in the apps subfolder
+  eleventyConfig.addCollection("appsAscending", (collection) =>
+    collection.getFilteredByGlob("apps/*.html").sort((a, b) => {
+      if (a.data.appTitle > b.data.appTitle) return 1;
+      else if (a.data.appTitle < b.data.appTitle) return -1;
+      else return 0;
+    })
+  );
+
   eleventyConfig.setTemplateFormats([
     "md",
     "html",
